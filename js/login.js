299bd13268a73e37462a5cb8666480c78343db89
paulo0056 <pauloandre.dev@gmail.com>
2022-08-30 21:08:37 -0300

    fix: update paths

--- js/login.js
+++ js/login.js
@@ -15,7 +15,7 @@ const handleSubmit = (e) => {
     e.preventDefault();
 
     localStorage.setItem('player', input.value);
-    window.location = './pages/game.html';
+    window.location = 'pages/game.html';
 }
 
 input.addEventListener('input', validateInput);
